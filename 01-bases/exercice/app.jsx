3c92c34cf4723e94a7ebc72935f567ea66a2e5e6
Matthieu Mota <matthieumota@gmail.com>
2023-01-04 15:05:21 +0100

    Exo JSX

--- 01-bases/exercice/app.jsx
+++ 01-bases/exercice/app.jsx
@@ -156,11 +156,128 @@ class Exo3 extends React.Component {
     }
 }
 
+class Exo4 extends React.Component {
+    render() {
+        const cat = '';
+
+        const persons = [
+            {
+                name: 'Charly',
+                age: 66,
+                job: {
+                    name: 'Styliste',
+                    since: '29 Avril 2020',
+                },
+            },
+            {
+                name: 'Charly',
+                age: 25,
+                job: {
+                    name: 'Barman',
+                    since: '29 Avril 5050',
+                },
+            }
+        ];
+
+        const shoppingCart = [
+            {
+                name: 'Concombre',
+                type: 'Legume',
+                warehouse: {
+                    name: 'MisterEpices',
+                    size: 'petit',
+                }
+            },
+            {
+                name: 'Courgette',
+                type: 'Legume',
+                warehouse: {
+                    name: 'MisterEpices',
+                    size: 'petit',
+                }
+            },
+            {
+                name: 'Endive',
+                type: 'Beurk',
+                warehouse: {
+                    name: 'Parking Souterrain',
+                    size: 'Grand',
+                }
+            },
+            {
+                name: 'Steak',
+                type: 'Frais',
+            },
+            {
+                name: 'Sanglier',
+                type: null,
+                warehouse: null,
+            }
+        ];
+
+        return (
+            <div>
+                <p>
+                    <a href="https://fr.reactjs.org/docs/conditional-rendering.html#inline-if-with-logical--operator">
+                        Documentation sur le rendu conditionnel à la volée en JSX
+                    </a>
+                </p>
+                <p>
+                    Après avoir consulté la documentation, afficher chat de manière conditionnelle seulement
+                    si son nom est différent de "Ronron" et qu'il n'est pas nul et avec la notation &&
+                </p>
+                {cat != 'Ronron' && cat && <p style={{ border: '1px solid #000' }}>{cat}</p>}
+                <p>
+                    Afficher les propriétés de chaque personne à l'aide de map
+                    Pour chaque personne afficher son âge de manière conditionnelle en utilisant ? et :
+                </p>
+                <p>
+                    Afficher les propriétés de chaque personne à l'aide de map
+                    Pour chaque personne afficher son âge de manière conditionnelle en utilisant ||
+                </p>
+                <ul>
+                    <li>
+                        Si l'âge de la personne est supérieur à 65, alors afficher "Senior"
+                    </li>
+                    <li>
+                        Sinon, afficher l'âge de la personne
+                    </li>
+                </ul>
+                {persons.map((person, index) =>
+                    <div key={index}>
+                        <h2>{person.name}</h2>
+                        <p>{person.job.name} depuis le {person.job.since}</p>
+                        {person.age > 65 ? <p>Senior</p> : <p>{person.age} ans</p>}
+                        {person.age > 65 && <p>Senior</p> || <p>{person.age} ans</p>}
+                        <p>{person.age > 65 ? 'Senior' : `${person.age} ans`}</p>
+                    </div>
+                )}
+                <p>Afficher tous les légumes dans une boucle map</p>
+                <p>Pour chaque légume, afficher son magasin de manière conditionnelle avec &&</p>
+
+                {shoppingCart.map((product, index) =>
+                    /*product.type == 'Legume' &&*/ <div key={index}>
+                        <h2>{product.name}</h2>
+                        <p style={{ color: product.type == 'Legume' ? 'green' : 'black' }}>{product.type}</p>
+                        {product.warehouse &&
+                            <div>
+                                <p>{product.warehouse.name}</p>
+                                <p>{product.warehouse.size}</p>
+                            </div>
+                        }
+                    </div>
+                )}
+            </div>
+        )
+    }
+}
+
 ReactDOM.render(
     <div>
         <Exo1 />
         <Exo2 />
         <Exo3 />
+        <Exo4 />
     </div>,
     document.querySelector('#root')
 );
