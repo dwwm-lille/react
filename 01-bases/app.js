ae8ee606f0d3b2a3fb69cc32286d37a02101136c
Matthieu Mota <matthieumota@gmail.com>
2023-01-04 09:43:07 +0100

    Conditions

--- 01-bases/app.js
+++ 01-bases/app.js
@@ -39,6 +39,7 @@ ReactDOM.render(
 
 // Exemple 4 avec composant
 // On crée le composant avec une classe (Solution 1)
+// Une classe contient uniquement des méthodes (fonctions)
 class Welcome extends React.Component {
     render() {
         return <h1>Bonjour {this.props.name}</h1>;
@@ -46,8 +47,9 @@ class Welcome extends React.Component {
 }
 
 // On peut créer un composant avec une fonction... (Solution 2)
-function Goodbye(props) {
-    return <h1>Au revoir {props.name}</h1>;
+// function Goodbye(props) {
+function Goodbye({ name }) {
+    return <h1>Au revoir {name}</h1>;
 }
 
 ReactDOM.render(
@@ -57,4 +59,41 @@ ReactDOM.render(
         <Goodbye name="React" />
     </div>,
     document.querySelector('#root4')
+);
+
+// Exemple 5 : Conditions
+class App extends React.Component {
+    displayFiorella() {
+        return <h1>Fiorella</h1>;
+    }
+
+    render() {
+        let kind = true;
+        let component = <Welcome name="App" />;
+
+        if (!kind) {
+            component = <Goodbye name="App" />;
+        }
+
+        return (
+            <div>
+                <h1>Un titre</h1>
+                {this.displayFiorella()}
+
+                <h2>Exemple 1 avec un If</h2>
+                {component}
+
+                <h2>Exemple 2 avec une condition à la volée</h2>
+                {kind && <Welcome name="App" />}
+                {!kind && <Goodbye name="App" />}
+
+                <h2>Exemple 3 avec un ternaire</h2>
+                {kind ? <Welcome name="App" /> : <Goodbye name="App" />}
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />, document.querySelector('#root5')
 );
