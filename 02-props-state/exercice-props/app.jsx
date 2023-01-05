19111be76cf0b4177d8be59242ff23a118f78734
Matthieu Mota <matthieumota@gmail.com>
2023-01-05 12:00:34 +0100

    Exo 3 props

--- 02-props-state/exercice-props/app.jsx
+++ 02-props-state/exercice-props/app.jsx
@@ -1,3 +1,4 @@
+// EXO 1 PROPS
 class Instruction1 extends React.Component {
     render() {
         return (
@@ -56,6 +57,7 @@ class Exo1 extends React.Component {
     }
 }
 
+// EXO 2 PROPS
 class Instruction2 extends React.Component {
     render() {
         return (
@@ -114,12 +116,117 @@ class Exo2 extends React.Component {
     }
 }
 
+// EXO 3 PROPS
+class Instruction3 extends React.Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <p>Créez un composant VegetablesCart et passez-lui en props les legumes.</p>
+                    <p>Dans le composant VegetablesCart, créer un composant Vegetable qui sera appelé autant de fois qu'il y a de légumes dans les props</p>
+                    <p>Dans le composant Vegetable, afficher le nom et le prix de chaque légume </p>
+                </div>
+                <div>
+                    <p>Créer un composant Students et passez-lui en props les élèves.</p>
+                    <p>Dans ce composant, créer un composant Student qui sera appelé autant de fois qu'il y a d'élèves dans les props</p>
+                    <p>Dans le composant Student, afficher l'élève dans une balise de votre choix</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+class Vegetable extends React.Component {
+    render() {
+        return <li>{this.props.vegetable.name} coûte {this.props.vegetable.price} €</li>;
+    }
+}
+
+class VegetablesCart extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Les légumes</h2>
+                <ul>
+                    {this.props.vegetables.map((vegetable, index) => <Vegetable key={index} vegetable={vegetable} />)}
+                </ul>
+            </div>
+        );
+    }
+}
+
+// @todo Students pour les explications
+
+class Exo3 extends React.Component {
+    render() {
+        const vegetables = listVegetables();
+        const students = listStudents();
+
+        return (
+            <div>
+                <TitleComponent />
+                <Instruction3 />
+                <VegetablesCart vegetables={vegetables} />
+            </div>
+        );
+    }
+}
+
+/**
+ * Cette fonction génère une liste de légumes en brut
+ */
+function listVegetables() {
+    return [
+        {
+            name: 'Topinambour',
+            price: 15,
+        },
+        {
+            name: 'Pois chiche',
+            price: 2,
+        },
+        {
+            name: 'Salade',
+            price: 999,
+        },
+        {
+            name: 'Courgette',
+            price: 1,
+        },
+        {
+            name: 'Oignon',
+            price: 2,
+        },
+    ]
+}
+
+/**
+ * Cette fonction génère une liste d'élèves en brut
+ */
+function listStudents() {
+    return [
+        {
+            name: 'Charly',
+            age: 15,
+        },
+        {
+            name: 'Bobbybillybouba',
+            age: 12,
+        },
+        {
+            name: 'JeanMichelDestroyer',
+            age: 16,
+        }
+    ]
+}
+
 class App extends React.Component {
     render() {
         return (
             <div>
                 <Exo1 />
                 <Exo2 />
+                <Exo3 />
             </div>
         )
     }
