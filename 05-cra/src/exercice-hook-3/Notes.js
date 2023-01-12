f01ef437b96229591ea2bafd826ed4eceb290a1f
Matthieu Mota <matthieumota@gmail.com>
2023-01-12 14:20:29 +0100

    Exo 3 hook

--- 05-cra/src/exercice-hook-3/Notes.js
+++ 05-cra/src/exercice-hook-3/Notes.js
@@ -1,3 +1,5 @@
+import { useMemo, useState } from "react";
+
 /**
  * Dispose d'un state notes, mis à jour avec setNotes
  * notes est un tableau d'entiers positifs, vide par défaut
@@ -5,6 +7,30 @@
  * Le formulaire propose d'ajouter une nouvelle note à la liste en utilisant setNotes
  */
 function Notes() {
+    let [notes, setNotes] = useState([]);
+    let [note, setNote] = useState(0);
+
+    // useMemo permet de mettre de "côté" le résultat de la fonction
+    // et cela appelle la fonction que si le tableau [notes] change
+    let average = useMemo(() => {
+        console.log('GO');
+        let result = notes.reduce((sum, note) => sum + note, 0) / notes.length;
+
+        return result.toFixed(2);
+    }, [notes]);
+
+    return (
+        <div>
+            {notes.length > 0 && <h1>Moyenne: {average}</h1>}
+            <ul>
+                {notes.map((note, index) =>
+                    <li key={index}>{note}</li>
+                )}
+            </ul>
+            <input type="number" value={note} onChange={(e) => setNote(e.target.value)} />
+            <button onClick={() => setNotes([ ...notes, parseInt(note) ])}>Ajouter</button>
+        </div>
+    );
 }
 
 export default Notes;
