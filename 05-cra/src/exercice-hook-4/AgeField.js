735aa574ab985c595c2f72ed72c26f3111f72f2d
Matthieu Mota <matthieumota@gmail.com>
2023-01-20 10:48:10 +0100

    Exo 4 hook

--- 05-cra/src/exercice-hook-4/AgeField.js
+++ 05-cra/src/exercice-hook-4/AgeField.js
@@ -1,9 +1,20 @@
+import { useState } from 'react';
+
 /**
  * State : age, mis à jour avec setAge
  * Affiche l'âge et un formulaire permettant de changer l'âge
- * Si l'âge est supérieur à 18, afficher "Wow, interdit aux mineurs ici !".
+ * Si l'âge est inférieur à 18, afficher "Wow, interdit aux mineurs ici !".
  */
 function AgeField() {
+    let [age, setAge] = useState('');
+
+    return (
+        <div>
+            {age && age < 18 && <p>Wow, interdit aux mineurs ici !</p>}
+            <label>Âge</label>
+            <input type="text" value={age} onChange={(e) => setAge(e.target.value)} />
+        </div>
+    );
 }
 
 export default AgeField;
--- 05-cra/src/exercice-hook-4/MailField.js
+++ 05-cra/src/exercice-hook-4/MailField.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 /**
  * Dispose d'un state mail mis à jour avec setMail
  * mail est une chaîne de caractères
@@ -8,6 +10,27 @@
  * Affiche une erreur si le mail n'a pas de format mail
  */
 function MailField() {
+    let [email, setEmail] = useState('');
+    let [error, setError] = useState(false);
+
+    useEffect(() => {
+        // Le .match va vérifier que l'email correspond à la regex (regex qui permet de vérifier un email valide)
+        if (email.length == 0 || email.match(/^\S+@\S+\.\S+$/)) {
+            setError(false); // L'email est valide s'il est vide ou que c'est un email correct
+        } else {
+            setError(true); // L'email n'est pas valide
+        }
+    }, [email]);
+
+    return (
+        <div>
+            <p>{email}</p>
+            {error && <p>L'email n'est pas valide</p>}
+
+            <label>Email</label>
+            <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </div>
+    );
 }
 
 export default MailField;
