1f2a509e48d6afc20be349675871b0edb9959424
Matthieu Mota <matthieumota@gmail.com>
2023-01-24 13:42:17 +0100

    Discover React Native

---
+++ 07-react-native/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, Image, Text, TouchableHighlight, View } from 'react-native';
+import styles from './style';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={[styles.title, styles.blue]}>Fiorella</Text>
+      <Image source={require('./assets/favicon.png')}
+             style={{ width: 100, height: 100, marginTop: 40 }} />
+      <Image source={{ uri: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png' }}
+             style={{ width: 150, height: 150 }} />
+
+      <Button title="Bonjour" onPress={() => alert('Salut Fiorella')} />
+
+      <TouchableHighlight style={styles.btn} underlayColor="red" onPress={() => alert('Mon bouton')}>
+        <Text style={{ color: '#fff', fontSize: 22 }}>Mon Bouton</Text>
+      </TouchableHighlight>
+
+      <StatusBar style="auto" hidden={false} />
+    </View>
+  );
+}
---
+++ 07-react-native/style.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 64,
+    fontFamily: 'Baskerville',
+  },
+  blue: {
+    color: 'blue',
+  },
+  btn: {
+    backgroundColor: 'blue',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 25,
+  },
+});
+
+export default styles;
