a8feb07dfdec0b713b8860a5107ecb62899f7a71
Matthieu Mota <matthieumota@gmail.com>
2023-01-25 12:10:21 +0100

    Lifecycle react native

--- 07-react-native/screens/Exercice1.js
+++ 07-react-native/screens/Exercice1.js
@@ -1,12 +1,29 @@
-import { useState } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { SafeAreaView, Text, View } from 'react-native';
 import styles from '../style';
 import SuperButton from '../components/Button';
 import { StatusBar } from 'expo-status-bar';
 import About from '../screens/About';
+import { useFocusEffect } from '@react-navigation/native';
 
 export default function Exercice1() {
   let [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log('MOUNT / UPDATE REACT LIFECYCLE');
+
+    return () => console.log('UNMOUNT REACT LIFECYCLE');
+  }, [count]);
+
+  // C'est comme le useEffect mais pour le cycle de vie de la navigation
+  useFocusEffect(useCallback(() => {
+    console.log('MOUNT NATIVE LIFECYCLE');
+
+    return () => {
+      console.log('UNMOUNT NATIVE LIFECYCLE');
+      setCount(0);
+    };
+  }, []));
 
   return (
     <SafeAreaView>
