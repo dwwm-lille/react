828b3ff0d746c9aadd11e66a4adc6f51112787c0
Matthieu Mota <matthieumota@gmail.com>
2023-01-30 11:45:36 +0100

    Item

--- 07-react-native/screens/Item.js
+++ 07-react-native/screens/Item.js
@@ -1,5 +1,13 @@
 import { Text } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
 
-export default function Item() {
-    return <Text>ITEM</Text>
+export default function Item({ route, navigation }) {
+    // A chaque fois qu'on ouvre un item, on change le titre du header par le nom de l'item
+    useFocusEffect(() => {
+        navigation.setOptions({ title: `Item ${route.params.number}` });
+    });
+
+    return (
+        <Text style={{ fontSize: 64 }}>{route.params.number}</Text>
+    )
 }
--- 07-react-native/screens/List.js
+++ 07-react-native/screens/List.js
@@ -4,12 +4,16 @@ import Button from '../components/Button';
 export default function List({ navigation }) {
     const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+    const goTo = (item) => {
+        navigation.navigate('Item', { number: item });
+    }
+
     return (
         <ScrollView>
             {items.map((item, index) =>
                 <View style={styles.item} key={index}>
                     <Text style={{ fontSize: 32 }}>
-                        <Button onPress={() => navigation.navigate('Item')}>
+                        <Button onPress={() => goTo(item)}>
                             {item}
                         </Button>
                     </Text>
